11696c35bb8f36748ccf671452761af2e773c025
Mateus S. Oliveira <mateusmateu17@gmail.com>
2022-05-25 17:58:33 -0300

    05.25

--- Front/script.js
+++ Front/script.js
@@ -26,6 +26,20 @@ $('section.awSlider2 .carousel').on('slid.bs.carousel', function () {
 	$('section.awSlider2 > img').attr('src',bscn2);
    });
 
+// Section 3
+
+$('section.awSlider3 .carousel').carousel({
+	pause: "hover",
+  interval: 2000
+});
+var startImage3 = $('section.awSlider3 .item.active > img').attr('src');
+$('section.awSlider3').append('<img src="' + startImage3 + '">');
+
+$('section.awSlider3 .carousel').on('slid.bs.carousel', function () {
+	var bscn2 = $(this).find('.item.active > img').attr('src');
+	$('section.awSlider3 > img').attr('src',bscn3);
+   });
+
 
 /* 
 Philips ambilight tv
